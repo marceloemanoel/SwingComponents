fe8d6700fa3d2677bc4141b2c8952e8a6f00de14
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2011-04-13 21:42:49 -0300

    refactoring on package structure

--- src/br/com/smartcoders/swingcomponents/table/model/sample/ContactTableModel.java
+++
@@ -1,8 +0,0 @@
-package br.com.smartcoders.swingcomponents.table.model.sample;
-
-import br.com.smartcoders.swingcomponents.table.model.AnnotationTableModel;
-
-@SuppressWarnings("serial")
-public class ContactTableModel extends AnnotationTableModel<Contact> {
-
-}
